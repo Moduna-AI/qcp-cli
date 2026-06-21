a6b9cf1c0278307ca4f091f2f55e09c93f9c8e50
Ashwin Thinnappan <sjashwin@live.in>
2026-06-21 20:59:52 +0530

    (chore:fix): distribution name compatbility

--- qcp/__init__.py
+++ qcp/__init__.py
@@ -3,6 +3,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("qcp")
+    __version__ = version("qcp-cli")
 except PackageNotFoundError:
     __version__ = "0+unknown"
